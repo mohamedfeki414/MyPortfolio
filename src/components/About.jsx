d9d3544a17ddbc9386310cc7c370f84c796cf475
feki_mohamed <medfeki33@gmail.com>
2024-12-02 17:19:11 +0100

    ajoute des modification

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,23 +14,17 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        Over the years, I've worked on a range of projects, 
+        from developing an e-commerce platform with React and Spring Boot to creating interactive web games with JavaScript. 
+        My passion lies in leveraging innovative technologies to build solutions that enhance user experiences.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+        I'm also certified in web development and Python/SQL.
+         My technical skills are complemented by soft skills such as teamwork, time management, and problem-solving.
+         When I'm not coding,I enjoy exploring technology trends, gaming, and following football.
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,14 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+          I'm Mohamed Feki, a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+          Currently in my third year pursuing a degree in Software Engineering and Information Systems at the
+           International Institute of Technology in Sfax, 
+           I have a strong foundation in web and mobile development.
+            My expertise spans technologies like React, ASP.NET Core, Spring Boot, and Flutter. 
+            I enjoy creating dynamic and interactive applications that solve real-world problems.
           </p>
 
           <div>
